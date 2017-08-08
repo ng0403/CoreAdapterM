396587784951d19fd7dab87c68420903c89e28c6
Core <Core@Core-PC>
2017-08-08 16:41:04 +0900

    titles 수정

--- AdapterM/src/main/java/com/core/plus/oppty/controller/OpptyController.java
+++ AdapterM/src/main/java/com/core/plus/oppty/controller/OpptyController.java
@@ -6,6 +6,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.core.plus.oppty.service.OpptyService;
@@ -19,7 +20,7 @@ public class OpptyController {
 	
 	// 처음 list 화면
 	@RequestMapping(value="oppty")
-	public ModelAndView oppty_list()
+	public ModelAndView opptyList()
 	{
 		List<OpptyVO> vo = opptyService.opptyList();
 		List<OpptyVO> status = opptyService.opptyStatusCD();
@@ -36,6 +37,16 @@ public class OpptyController {
 		mov.addObject("oppty_stage_cd", stage);
 		mov.addObject("dtype_cd", dtype);
 		mov.addObject("purchase_type", purchase);
+		
+		return mov;
+	}
+	
+	@RequestMapping(value="oppty_detail")
+	public ModelAndView opptyDetail(@RequestParam("oppty_no") String oppty_no)
+	{
+		System.out.println("detail : " + oppty_no);
+		
+		ModelAndView mov = new ModelAndView("oppty_detail");
 		
 		return mov;
 	}
